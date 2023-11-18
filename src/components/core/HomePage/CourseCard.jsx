e334e5dfe4d07f82aeb1cd657c7bef20563dcdaf
Sahil Sajwan <cls>
2023-11-18 22:28:55 +0530

    fifth commit

--- src/components/core/HomePage/CourseCard.jsx
+++ src/components/core/HomePage/CourseCard.jsx
@@ -1,7 +1,14 @@
-function CourseCard(){
+function CourseCard({cardData,currentCard,setCurrentCard}){
     return(
         <div>
-
+            <div className="flex flex-col text-white">
+                <div >{cardData.heading}</div>
+                <div>{cardData.description}</div>
+                <div>
+                    <div>{cardData.level}</div>
+                    <div>{cardData.lessionNumber}</div>
+                </div>
+            </div>
         </div>
     )
 }
