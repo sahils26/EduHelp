305c19da63132e2e49c98734de3fd7e97d4a5765
Sahil Sajwan <cls>
2024-01-29 11:43:24 +0530

    29/1/24

--- src/components/core/Dashboard/AddCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -12,15 +12,14 @@ function AddCourse(){
             <div>
                 <p>Code Upload Tips</p>
                 <ul>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
-                    <li>Code .............</li>
+                    <li>Set the Course Price option or make it free.</li>
+                    <li>Standard size for the course thumbnail is 1024x576.</li>
+                    <li>Video section controls the course overview video.</li>
+                    <li>Course Builder is where you create & organize a course.</li>
+                    <li>Add Topics in the Course Builder section to create lessons, quizzes, and assignments.</li>
+                    <li>Information from the Additional Data section shows up on the course single page.</li>
+                    <li>Make Announcements to notify any important</li>
+                    <li>Notes to all enrolled students at once.</li>
                 </ul>
             </div>
         </div>
