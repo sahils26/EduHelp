aa31e126f4fea156f097ee901b73ad968f50e948
Sahil Sajwan <sahilsajwan26@gmail.com>
2025-03-12 02:54:01 +0100

    Replace bcrypt with bcryptjs for better deployment compatibility

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,5 +1,6 @@
 const User=require("../models/User");
-const bcrypt=require("bcrypt")
+// const bcrypt=require("bcrypt")
+const bcrypt = require('bcryptjs');
 const mailSender = require("../utils/mailSender")
 
 
