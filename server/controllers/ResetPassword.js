730bf6ffa83b0eb2305043d06fc257fc91ad0b28
Sahil Sajwan <sahilsajwan26@gmail.com>
2025-03-17 04:21:38 +0100

    claude's resetPasswordToken

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -6,52 +6,121 @@ const mailSender = require("../utils/mailSender")
 
 
 
-exports.resetPasswordToken= async(req,res)=> {
-    try{
-        console.log("Inside resetPasswordToken")
-        const email=req.body.email;
+// exports.resetPasswordToken= async(req,res)=> {
+//     try{
+//         console.log("Inside resetPasswordToken")
+//         const email=req.body.email;
         
-        const user= await User.findOne({email:email});
-        if(!user){
+//         const user= await User.findOne({email:email});
+//         if(!user){
+//             return res.json({
+//                 success:false,
+//                 message:'Email not registered '
+//             })
+//         }
+        
+//         const token=crypto.randomUUID();
+        
+//         const updatedData= await User.findOneAndUpdate({email:email},
+//             {
+//                 token:token,
+//                 resetPasswordExpires: Date.now() + 3600000,
+//             })
+            
+            
+//             const url=`http://localhost:3000/update-password/${token}`;
+            
+//             await mailSender(
+//                 email,
+//                 "Password Reset",
+//                 `Your Link for email verification is ${url}. Please click this url to reset your password.`
+//               )            
+//         return res.json({
+//             success:true,
+//             message:'Email send Successfully, check email to send Password'
+//         });
+
+//     }catch(error){
+//         console.log(error);
+//         res.status(500).json({
+//             success:false,
+//             message:'something went wrong while resetting password'
+//         })
+//     }
+// }
+
+
+
+
+
+
+
+
+
+//claude's code
+exports.resetPasswordToken = async(req, res) => {
+    try {
+        console.log("Inside resetPasswordToken")
+        const email = req.body.email;
+       
+        const user = await User.findOne({email: email});
+        if(!user) {
             return res.json({
-                success:false,
-                message:'Email not registered '
+                success: false,
+                message: 'Email not registered '
             })
         }
-        
-        const token=crypto.randomUUID();
-        
-        const updatedData= await User.findOneAndUpdate({email:email},
+       
+        const token = crypto.randomUUID();
+       
+        const updatedData = await User.findOneAndUpdate(
+            {email: email},
             {
-                token:token,
+                token: token,
                 resetPasswordExpires: Date.now() + 3600000,
-            })
-            
-            
-            const url=`http://localhost:3000/update-password/${token}`;
-            
-            await mailSender(
-                email,
-                "Password Reset",
-                `Your Link for email verification is ${url}. Please click this url to reset your password.`
-              )            
+            }
+        );
+           
+        // Change this line - Use environment variable or check the request origin
+        // Instead of hardcoded localhost URL
+        const frontendURL = process.env.FRONTEND_URL || 'https://edu-help-six.vercel.app';
+        const url = `${frontendURL}/update-password/${token}`;
+           
+        await mailSender(
+            email,
+            "Password Reset",
+            `Your Link for email verification is ${url}. Please click this url to reset your password.`
+        );            
+        
         return res.json({
-            success:true,
-            message:'Email send Successfully, check email to send Password'
+            success: true,
+            message: 'Email sent successfully, check email to reset password'
         });
-
-    }catch(error){
+    } catch(error) {
         console.log(error);
-        res.status(500).json({
-            success:false,
-            message:'something went wrong while resetting password'
-        })
+        return res.status(500).json({
+            success: false,
+            message: 'Something went wrong while resetting password'
+        });
     }
 }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+//mohan's code
 // // resetPasswordToken
 // exports.resetPasswordToken = async(req, res) => {
 //     console.log("object server")
